e2ae8c8339a9ef16df53c04a3a40798e585f6c62
Torsten Freyhall <torsten@freyhall.se>
2016-04-04 14:43:07 +0200

    project: Added standalone build command for building self-contained library.

---
+++ src/standalone-build.js
@@ -0,0 +1,6 @@
+if (typeof window.angular !== 'undefined') {
+  window.angular.module('metry', [])
+  .factory('mryDateUtil', function() { return require('./util/date-util.js'); })
+  .factory('mryAuth', require('./auth.js'))
+  .factory('mry', require('./metry-sdk.js'));
+}
