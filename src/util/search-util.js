da880247df9c44504c92e9fd520d0481cfa145db
Torsten Freyhall <torsten@freyhall.se>
2016-05-06 10:39:30 +0200

    util: Removed parse from search-util as it was specific to user portal

--- src/util/search-util.js
+++ src/util/search-util.js
@@ -1,47 +1,22 @@
 var ObjectUtil = require('./object-util.js');
 
-function queryParse(queryString) {
-  return queryString
-    .split('AND')
-    .map(function(s) {
-      var components = s.split(':');
-      if (components.length !== 2) { return null; }
-      var key = components[0].trim();
-      var value = components[1].trim();
-      return {
-        key: key,
-        value: value,
-        tag: key + ': ' + value,
-        autocomplete: {
-          key: key,
-          value: value,
-          type: key
-        }
-      };
-    })
-    .filter(function(c) { return c !== null; });
-}
-
-function queryTransform(queryArray) {
+// Converts an array of objects with {key, value} to a query-string on format
+// q=key1:value1 AND key2:value2 AND value3
+// If key is '' then only value is appended.
+// If a value is null, undefined, or empty string, it is ignored
+exports.makeQuery = function makeQuery(queryArray) {
   if (!ObjectUtil.isArray(queryArray) || queryArray.length === 0) {
     return null;
   }
-  return queryArray.filter(function(q) {
-    return (q &&
-            'key' in q &&
-            'value' in q &&
-            q.value !== null &&
-            typeof q.value !== 'undefined' &&
-            (typeof q.value !== 'string' || q.value.length > 0));
-  })
-  .map(function(q) {
-    return (q.key.length === 0) ? q.value : [q.key, q.value].join(':');
-  })
-  .filter(function(q) { return q !== null; })
-  .join(' AND ');
-}
-
-module.exports = {
-  parse: queryParse,
-  makeQuery: queryTransform
+  return queryArray
+    .filter(function(q) {
+      return (q && 'key' in q && 'value' in q && q.value !== null &&
+              typeof q.value !== 'undefined' &&
+              (typeof q.value !== 'string' || q.value.length > 0));
+    })
+    .map(function(q) {
+      return (q.key.length === 0) ? q.value : [q.key, q.value].join(':');
+    })
+    .filter(function(q) { return q !== null; })
+    .join(' AND ');
 };
